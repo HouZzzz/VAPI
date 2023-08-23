ac5c72fea53a982f1d22e12c0ef601829f666230
HouZzzz <hozzchef@gmail.com>
2023-08-23 21:56:00 +0300

    initional commit

---
+++ src/main/java/org/vapi/Main.java
@@ -0,0 +1,7 @@
+package org.vapi;
+
+public class Main {
+    public static void main(String[] args) {
+        ValorantAPI valorantAPI = new ValorantAPI();
+    }
+}
---
+++ src/main/java/org/vapi/ValorantAPI.java
@@ -0,0 +1,9 @@
+package org.vapi;
+
+public class ValorantAPI {
+    private String key;
+
+    public ValorantAPI() {}
+
+    public ValorantAPI(String key) {this.key = key;}
+}
